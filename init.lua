84e1e0008d17f4f4b5744554b1141c6e57d2b770
Tharindu Pramuditha <tharindutpk@gmail.com>
2023-10-26 15:23:15 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -348,38 +348,38 @@ vim.wo.signcolumn = 'yes'              -- Keep signcolumn on by default
 
 -- [[ Basic Keymaps ]]
 
--- Escape using jk in insert and terminal mode
-vim.keymap.set('i', 'jk', '<ESC>')
-vim.keymap.set('t', 'jk', '<C-\\><C-n>')
+-- -- Escape using jk in insert and terminal mode
+-- vim.keymap.set('i', 'jk', '<ESC>')
+-- vim.keymap.set('t', 'jk', '<C-\\><C-n>')
 
 -- Keymaps for better default experience
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+-- vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+-- vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+-- vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- [[ Highlight on yank ]]
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
+-- local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+-- vim.api.nvim_create_autocmd('TextYankPost', {
+--   callback = function()
+--     vim.highlight.on_yank()
+--   end,
+--   group = highlight_group,
+--   pattern = '*',
+-- })
 
 -- [[ Configure Telescope ]]
-require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
-    },
-  },
-}
+-- require('telescope').setup {
+--   defaults = {
+--     mappings = {
+--       i = {
+--         ['<C-u>'] = false,
+--         ['<C-d>'] = false,
+--       },
+--     },
+--   },
+-- }
 
 -- Enable telescope fzf native, if installed
 -- pcall(require('telescope').load_extension, 'fzf')
